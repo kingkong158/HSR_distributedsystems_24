a7decc584a3fbdee101ce286f83c4dcde8ef55fa
kingkong158 <163885775+kingkong158@users.noreply.github.com>
2024-06-29 09:04:07 +0000

    created the table for fusballspielerKarte

--- todo-database/init.sql
+++ todo-database/init.sql
@@ -1,11 +1,28 @@
-CREATE SEQUENCE todo_item_seq START 1;
+CREATE SEQUENCE fusballspieler_karte_seq START 1;
 
-CREATE TABLE IF NOT EXISTS todo_item (
-    todo_item_id SERIAL PRIMARY KEY,
-    todo_item_name VARCHAR(50) NOT NULL,
-    todo_item_description VARCHAR(255),
-    due_date TIMESTAMP
+CREATE TABLE IF NOT EXISTS fusballspielerKarte (
+    karten_nr SERIAL PRIMARY KEY,
+    spielername VARCHAR(50) NOT NULL,
+    manschaft VARCHAR(50) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    marktwert INT
 );
 
-INSERT INTO todo_item(todo_item_name, todo_item_description)
-VALUES ('Todo Test 1', 'Dies ist eine Test Aufgabe'); 
+INSERT INTO fusballspielerKarte(spielername, manschaft, position, marktwert)
+VALUES ('Thomas Mueller', 'Deutschland', 'Mittelfeld', '100000000'); 
+
+INSERT INTO fusballspielerKarte(spielername, manschaft, position, marktwert)
+VALUES ('Phillip Lahm', 'Deutschland', 'Verteidigung', '999999999'); 
+
+INSERT INTO fusballspielerKarte(spielername, manschaft, position, marktwert)
+VALUES ('Manuel Neuer', 'Deutschland', 'Torwart', '120000000');
+
+INSERT INTO fusballspielerKarte(spielername, manschaft, position, marktwert)
+VALUES ('Lionel Messi', 'Argentinien', 'Sturm', '200000000');
+
+INSERT INTO fusballspielerKarte(spielername, manschaft, position, marktwert)
+VALUES ('Christiano Ronaldo', 'Portugal', 'Sturm', '200000000');
+
+--SELECT * FROM fusballspielerKarte;
+
+--DELETE FROM fusballspielerKarte WHERE spielername = 'Lionel Messi';
